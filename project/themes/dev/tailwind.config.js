64b75e0c4f1875e3d3f1ebddf1a30d62779cb9cc
Sergey Romanenko <sergey.romanenko@flextype.org>
2022-02-23 17:40:32 +0300

    update content, templates and styles - next round

--- project/themes/dev/tailwind.config.js
+++ project/themes/dev/tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
   plugins: [
     require('postcss-import'),
     require('tailwindcss'),
-    require('autoprefixer')
+    require('autoprefixer'),
+    require('prettier-plugin-tailwindcss')
   ]
 }
