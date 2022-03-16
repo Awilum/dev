458a818fb0427045a16c934ab6a27337c423cd0d
Sergey Romanenko <sergey.romanenko@flextype.org>
2022-03-16 20:23:38 +0300

    update static site generator script

--- project/plugins/site/routes/web.php
+++ project/plugins/site/routes/web.php
@@ -19,3 +19,51 @@ flextype('emitter')->addListener('onFlextypeBeforeRun', static function (): void
               ->setName('site.index')
               ->add('csrf');
 });
+
+
+flextype()->get('/generate-static-site', function () {
+
+    // Set static site directory path
+    $staticSitePath = '../awilum.github.io/';
+
+    // Set static site base url
+    //flextype('registry')->set('flextype.settings.url', 'CUSTOM_BASE_URL');
+
+    // Get entries list
+    $entriesFiles = filesystem()->find()->files()->in(PATH['project'] . '/entries/');
+
+    // Create static site directory
+    filesystem()->directory($staticSitePath)->create(0755, true);
+
+    // Create static site entries
+    $i = 0;
+    foreach($entriesFiles as $file) {
+        $i++;
+        $entryID = strings($file->getPath() . '/' . $file->getFilename())->replace('/entry.md', '')->replace(ROOT_DIR . '/project/entries/', '')->toString(); 
+        $entry = flextype('entries')->fetch($entryID);
+        echo $i . ' - ' . $entryID  . "<br>";
+        filesystem()->directory($staticSitePath . $entryID)->create(0755, true);
+        filesystem()
+            ->file($staticSitePath . $entryID . '/index.html')
+            ->put(flextype('twig')->fetch('themes/' . flextype('registry')->get('plugins.site.settings.theme') . '/' . (empty($entry['template']) ? 'templates/default' : 'templates/' . $entry['template']) . '.html', ['entry' => $entry]));
+    }
+    
+    // Create static site project index.html for main main entry
+    filesystem()->file($staticSitePath . '/index.html')->put(file_get_contents($staticSitePath . '/' . flextype('registry')->get('plugins.site.settings.entries.main') . '/index.html'));
+    filesystem()->directory($staticSitePath . '/' . flextype('registry')->get('plugins.site.settings.entries.main'))->delete();
+    
+    // Create empty index.html file in static site project directory
+    filesystem()->file($staticSitePath . '/project/index.html')->put('');
+    
+    // Create assets directory and copy all assets into the static site directory
+    filesystem()->directory($staticSitePath . 'project/themes/' . flextype('registry')->get('plugins.site.settings.theme') . '/assets/')->create(0755, true);
+    filesystem()->directory(PATH['project'] . '/themes/' . flextype('registry')->get('plugins.site.settings.theme') . '/assets/')->copy($staticSitePath . 'project/themes/' . flextype('registry')->get('plugins.site.settings.theme') . '/assets/');
+
+    // Create sitemap
+    filesystem()->file(ROOT_DIR . '/sitemap.xml')->copy($staticSitePath . '/sitemap.xml');
+    filesystem()->directory($staticSitePath . '/project/plugins/sitemap/templates/')->create(0755, true);
+    filesystem()->file(PATH['project'] . '/plugins/sitemap/templates/sitemap.xsl')->copy($staticSitePath . '/project/plugins/sitemap/templates/sitemap.xsl');
+
+    die('Done :)');    
+});
+
